c015bee57904251446715ebb2d4b59c9a4ccc003
Himmmera <120020624+Himmmera@users.noreply.github.com>
2023-08-16 18:53:32 +0700

    Update MobileBankApiTestV1.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,12 +1,14 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matcher.*;
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldReturnDemoAccountsStatusCode() {
         // Given - When - Then
         // Предусловия
         given()
